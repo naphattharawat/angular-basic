504a7dd4e2794882dcba6d47676963d1c392ed8e
beerIT15kmutnb <beerde789@hotmail.com>
2021-11-18 16:09:17 +0700

    update

--- src/app/admin/services/users.service.ts
+++ src/app/admin/services/users.service.ts
@@ -6,13 +6,28 @@ import { HttpClient } from "@angular/common/http";
 export class UsersService {
 
   constructor(
-    @Inject('API_URL') private apiUrl: string,
-    private httpClient: HttpClient,
+    // @Inject('API_URL') private apiUrl: string,
+    private httpClient: HttpClient
   ) { }
 
   async getUser() {
-    const url = `${this.apiUrl}/users`;
-    return await this.httpClient.get(url).toPromise()
+    const url = `http://localhost:3000/users`;
+    return await this.httpClient.get(url).toPromise();
   }
 
+  async update(id: number, json: any) {
+    // const url = 'http://localhost:3000/users/' + id;
+    const url = `http://localhost:3000/users/${id}`;
+    return await this.httpClient.put(url, json).toPromise();
+  }
+
+  async remove(id: number) {
+    const url = `http://localhost:3000/users/${id}`;
+    return await this.httpClient.delete(url).toPromise();
+  }
+
+  async save(json: any) {
+    const url = `http://localhost:3000/users`;
+    return await this.httpClient.post(url,json).toPromise();
+  }
 }
--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/alert.service';
 import { UsersService } from '../services/users.service';
+
 
 @Component({
   selector: 'app-users',
@@ -9,8 +11,16 @@ import { UsersService } from '../services/users.service';
 export class UsersComponent implements OnInit {
 
   list = [];
+  name!: string;
+  lname: any;
+  username!: string;
+  password!: string;
+  id!: number;
+  modalEditUser = false;
+  isUpdate = false;
   constructor(
-    private usersService: UsersService
+    private usersService: UsersService,
+    private alertService: AlertService
   ) { }
 
   ngOnInit(): void {
@@ -24,13 +34,74 @@ export class UsersComponent implements OnInit {
         this.list = rs.rows;
       } else {
         console.log(rs.error);
-
       }
     } catch (error) {
       console.log(error);
-
     }
   }
 
+  onClickEdit(i: any) {
+    this.isUpdate = true;
+    this.name = i.first_name;
+    this.lname = i.last_name;
+    this.id = i.id;
+    this.modalEditUser = true;
+  }
 
+  async onClickSave() {
+    try {
+      const json: any = {
+        "first_name": this.name,
+        "last_name": this.lname,
+        "username": this.username,
+        "title_id": 2,
+        "password": this.password
+      }
+      if (this.isUpdate) {
+        const rs: any = await this.usersService.update(this.id, json);
+        if (rs.ok) {
+          this.modalEditUser = false;
+          this.getList();
+        } else {
+          console.log(rs);
+        }
+      } else {
+        const save: any = await this.usersService.save(json);
+        if (save.ok) {
+          this.modalEditUser = false;
+          this.getList();
+          this.alertService.success();
+        } else {
+          this.alertService.error(save.error);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async onClickRemove(i: any) {
+    try {
+      const confirm = await this.alertService.confirm();
+      if (confirm.isConfirmed) {
+        const rs: any = await this.usersService.remove(i.id);
+        if (rs.ok) {
+          this.getList();
+        } else {
+          console.log(rs);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  onClickAdd() {
+    this.isUpdate = false;
+    this.name = '';
+    this.lname = '';
+    this.username = '';
+    this.password = '';
+    this.modalEditUser = true;
+  }
 }
